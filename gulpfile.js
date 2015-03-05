2e300b3b547c388c2690616abc62b7796297624f
Wildhoney <adam.timberlake@gmail.com>
2015-03-05 22:13:50 +0000

    Added blueprint YAML config file

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,17 @@
         karma      = require('gulp-karma'),
         rename     = require('gulp-rename'),
         fs         = require('fs'),
+        yaml       = require('js-yaml'),
         browserify = require('browserify'),
         babelify   = require('babelify');
 
-    /**
-     * @property entryFile
-     * @type {String}
-     */
-    var entryFile = './src/Blueprint.js';
+    // Load the YAML configuration file.
+    var config = yaml.safeLoad(fs.readFileSync('./.blueprint.yml', 'utf8'));
 
-    /**
-     * @property allFiles
-     * @type {String[]}
-     */
-    var allFiles = ['./src/*.js', './src/**/*.js', './src/**/**/*.js'];
+    // Common entry values.
+    var entryFile = config.gulp.entry,
+        allFiles  = config.gulp.all,
+        prodPath  = config.gulp.directories.dist + '/' + config.gulp.names.prod;
 
     /**
      * @method buildTo
@@ -30,50 +27,48 @@
     var buildTo = function(destPath) {
 
         return browserify({ debug: true })
-            .transform(babelify)
-            .require(entryFile, { entry: true })
-            .bundle()
-            .on('error', function (model) { console.error(['Error:', model.message].join(' ')); })
-            .pipe(fs.createWriteStream(destPath));
+                .transform(babelify)
+                .require(entryFile, { entry: true })
+                .bundle()
+                .on('error', function (model) { console.error(['Error:', model.message].join(' ')); })
+                .pipe(fs.createWriteStream(destPath));
 
     };
 
     gulp.task('compile', function() {
-        return buildTo('dist/blueprint.js');
+        return buildTo(prodPath);
     });
 
     gulp.task('minify', ['compile'], function() {
 
-        return gulp.src('dist/blueprint.js')
+        return gulp.src(prodPath)
                    .pipe(uglify())
-                   .pipe(rename('blueprint.min.js'))
-                   .pipe(gulp.dest('dist'));
+                   .pipe(rename(config.gulp.names.dev))
+                   .pipe(gulp.dest(config.gulp.directories.dist));
 
     });
 
     gulp.task('vendorify', ['compile'], function() {
-        return gulp.src('dist/blueprint.js').pipe(gulp.dest('public/vendor/blueprint'));
+        return gulp.src(prodPath).pipe(gulp.dest(config.gulp.directories.vendor));
     });
 
     gulp.task('lint', function() {
 
         return gulp.src(allFiles)
-            .pipe(jshint())
-            .pipe(jshint.reporter('default', {
-                verbose: true
-            }));
+                   .pipe(jshint())
+                   .pipe(jshint.reporter('default', {
+                       verbose: true
+                   }));
 
     });
 
     gulp.task('karma-prepare', function() {
-        return buildTo('tests/build/blueprint.js');
+        return buildTo(config.gulp.tests.temp);
     });
 
     gulp.task('karma', ['karma-prepare'], function() {
 
-        var deps = ['public/vendor/d3/d3.js', 'public/vendor/lodash/lodash.js'];
-
-        return gulp.src(deps.concat(['tests/*.test.js', 'tests/build/blueprint.js']))
+        return gulp.src(config.gulp.dependencies.concat(['tests/*.test.js', config.gulp.tests.temp]))
                    .pipe(karma({
                         configFile: 'karma.conf.js',
                         action: 'run'
@@ -85,7 +80,7 @@
     });
 
     gulp.task('karma-clean', ['karma'], function () {
-        return gulp.src('tests/build/blueprint.js', { read: false }).pipe(clean());
+        return gulp.src(config.gulp.tests.temp, { read: false }).pipe(clean());
     });
 
     gulp.task('test', ['lint', 'karma-prepare', 'karma', 'karma-clean']);
