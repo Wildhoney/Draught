bc6e58f050894d641aabfde9967c8a933d482485
Wildhoney <adam.timberlake@gmail.com>
2015-06-24 15:22:49 +0100

    Added test case for options

--- tests/Draft.test.js
+++ tests/Draft.test.js
@@ -1,13 +1,23 @@
-import Draft from '../src/Draft.js';
+import Draft   from '../src/Draft.js';
+import Symbols from '../src/helpers/Symbols.js';
 
 describe('Draft', () => {
 
     const mockSVGElement = document.createElement('svg');
+    const getDraft       = (options) => new Draft(mockSVGElement, options);
 
     it('Should be able to instantiate the constructor function;', () => {
+        expect(getDraft() instanceof Draft).toBeTruthy();
+    });
 
-        const draft = new Draft(mockSVGElement);
-        expect(draft instanceof Draft).toBeTruthy();
+    it('Should be able to pass in options that override the defaults;', () => {
+
+        const draft   = getDraft({ documentHeight: '50%', gridSize: 5 });
+        const options = draft[Symbols.options];
+
+        expect(options.documentHeight).toEqual('50%');
+        expect(options.documentWidth).toEqual('100%');
+        expect(draft.getOptions().gridSize).toEqual(5);
 
     });
 
