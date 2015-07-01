13f6cec68eca572c5b519be8ee1ae3554b4bc947
Wildhoney <adam.timberlake@gmail.com>
2015-07-01 13:35:58 +0100

    Added tests for selecting/deselecting shapes

--- tests/abilities/Selectable.test.js
+++ tests/abilities/Selectable.test.js
@@ -15,11 +15,31 @@ describe('Selectable', () => {
 
         spyOn(shapes.first, 'didSelect').and.callThrough();
         spyOn(shapes.second, 'didSelect').and.callThrough();
+        spyOn(shapes.first, 'didDeselect').and.callThrough();
+        spyOn(shapes.second, 'didDeselect').and.callThrough();
 
         draft.selectShapes(shapes.first);
         expect(shapes.first.didSelect.calls.count()).toEqual(1);
         expect(shapes.second.didSelect.calls.count()).toEqual(0);
         expect(shapes.first.isSelected()).toBeTruthy();
+        expect(shapes.second.isSelected()).toBeFalsy();
+
+        draft.selectShapes();
+        expect(shapes.first.didSelect.calls.count()).toEqual(2);
+        expect(shapes.second.didSelect.calls.count()).toEqual(1);
+        expect(shapes.first.isSelected()).toBeTruthy();
+        expect(shapes.second.isSelected()).toBeTruthy();
+
+        draft.deselectShapes(shapes.second);
+        expect(shapes.first.didDeselect.calls.count()).toEqual(0);
+        expect(shapes.second.didDeselect.calls.count()).toEqual(1);
+        expect(shapes.first.isSelected()).toBeTruthy();
+        expect(shapes.second.isSelected()).toBeFalsy();
+
+        draft.deselectShapes();
+        expect(shapes.first.didDeselect.calls.count()).toEqual(1);
+        expect(shapes.second.didDeselect.calls.count()).toEqual(2);
+        expect(shapes.first.isSelected()).toBeFalsy();
         expect(shapes.second.isSelected()).toBeFalsy();
 
     });
